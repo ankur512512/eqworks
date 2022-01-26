4fe89afb0b3a061848aa3dbb968c58a412ae2a04
Ankur Garg <ankurgarg1203@gmail.com>
2022-01-26 18:00:01 +0530

    Improving populate.sh script

--- populate.sh
+++ populate.sh
@@ -10,17 +10,26 @@ localexport() {
 #Checking if directory and mysqldump already exists
 if [ -d "$hostdir" ]; then
 echo "Directory already exists" ;
-  if [ -z "$(ls -A $hostdir)" ]; then
+  if [ ! -f "$hostdir/mysqldump.sql" ]; then
      echo "Empty. Populating this directory using postgresql client."
      localexport
+  else
+     echo -e "\n Found existing mysql.dump file, so using that and starting the docker containers...\n"
   fi
 
 ## Creating directory and populating with mysqlpdump
 else
-`mkdir -p $hostdir`;
-echo -e "$hostdir directory is created.\n Now populating $hostdir with mysqldump"
-localexport
+  mkdir -p $hostdir
+  echo -e "$hostdir directory is created.\n Now populating $hostdir with mysqldump"
+  localexport
 fi
 
 echo -e "Running docker-compose command now... make sure you have docker-compose command installed and available in your PATH\n"
 docker-compose up --build -d
+
+echo -e "\nSleeping for 15 seconds to give time to containers to get ready to accept connections..."
+sleep 15
+
+echo -e "\nDoing curl on localhost:5000/poi to see if everything is working fine...\n"
+curl localhost:5000/poi
+
